a931562971db873814d85d4182e6f34a15e7ed7e
chooblarin <choo.bla.rin@gmail.com>
2015-12-06 18:00:00 +0900

    Show timeline

--- app/src/main/java/com/chooblarin/githublarin/service/GitHubApiService.java
+++ app/src/main/java/com/chooblarin/githublarin/service/GitHubApiService.java
@@ -8,40 +8,24 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
-import com.chooblarin.githublarin.BuildConfig;
+import com.chooblarin.githublarin.api.ApiClientProvider;
 import com.chooblarin.githublarin.api.auth.Credential;
 import com.chooblarin.githublarin.api.client.GitHubClient;
-import com.chooblarin.githublarin.api.http.Header;
 import com.chooblarin.githublarin.api.response.SearchResponse;
-import com.chooblarin.githublarin.model.Entry;
 import com.chooblarin.githublarin.model.Gist;
 import com.chooblarin.githublarin.model.Repository;
 import com.chooblarin.githublarin.model.User;
-import com.chooblarin.githublarin.model.FeedParser;
-import com.google.gson.Gson;
 import com.squareup.okhttp.Credentials;
-import com.squareup.okhttp.Interceptor;
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
-import com.squareup.okhttp.logging.HttpLoggingInterceptor;
 
-import java.io.IOException;
 import java.util.List;
 
-import retrofit.GsonConverterFactory;
-import retrofit.Retrofit;
-import retrofit.RxJavaCallAdapterFactory;
 import rx.Observable;
 
 public class GitHubApiService extends Service {
 
     private final static String TAG = "GitHubApiService";
 
-    public final static String GITHUB_BASE_URL = "https://api.github.com";
-
     private IBinder binder = new GitHubApiBinder();
-    private OkHttpClient okHttpClient;
     private GitHubClient gitHubClient;
     private User user;
 
@@ -70,13 +54,13 @@ public class GitHubApiService extends Service {
             if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)) {
                 authorization = Credentials.basic(username, password);
             }
-            gitHubClient = createGitHubApiClient(authorization);
+            gitHubClient = ApiClientProvider.gitHubClient(authorization);
         }
     }
 
     public Observable<User> login(String username, String password) {
         String authCredential = Credentials.basic(username, password);
-        gitHubClient = createGitHubApiClient(authCredential);
+        gitHubClient = ApiClientProvider.gitHubClient(authCredential);
         return gitHubClient.user(username);
     }
 
@@ -102,58 +86,5 @@ public class GitHubApiService extends Service {
 
     public void setUser(User user) {
         this.user = user;
-    }
-
-    public Observable<List<Entry>> feeds() {
-        return gitHubClient.feeds()
-                .map(feedsResponse -> feedsResponse.currentUserUrl)
-                .flatMap(url -> Observable.create((Observable.OnSubscribe<Response>) subscriber -> {
-                    Request request = new Request.Builder().url(url).build();
-                    try {
-                        Response response = okHttpClient.newCall(request).execute();
-                        subscriber.onNext(response);
-                    } catch (IOException e) {
-                        subscriber.onError(e);
-                    }
-                    subscriber.onCompleted();
-                }))
-                .map(response -> {
-                    String bodyText = null;
-                    try {
-                        bodyText = response.body().string();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    return bodyText;
-                })
-                .map(FeedParser::parseString);
-    }
-
-    private GitHubClient createGitHubApiClient(@Nullable String authorization) {
-        okHttpClient = new OkHttpClient();
-        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
-        interceptor.setLevel(BuildConfig.DEBUG ?
-                HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE);
-        okHttpClient.interceptors().add(interceptor);
-        okHttpClient.interceptors().add(new Interceptor() {
-            @Override
-            public Response intercept(Chain chain) throws IOException {
-                Request request = chain.request();
-                Request.Builder builder = request.newBuilder();
-                builder.addHeader(Header.ACCEPT, "application/json");
-                if (null != authorization) {
-                    builder.addHeader(Header.AUTHORIZATION, authorization);
-                }
-                return chain.proceed(builder.build());
-            }
-        });
-
-        return new Retrofit.Builder()
-                .client(okHttpClient)
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create(new Gson()))
-                .baseUrl(GITHUB_BASE_URL)
-                .build()
-                .create(GitHubClient.class);
     }
 }
--- app/src/main/java/com/chooblarin/githublarin/ui/fragment/EventFragment.java
+++ app/src/main/java/com/chooblarin/githublarin/ui/fragment/EventFragment.java
@@ -1,29 +1,60 @@
 package com.chooblarin.githublarin.ui.fragment;
 
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.chooblarin.githublarin.R;
+import com.chooblarin.githublarin.api.ApiClientProvider;
+import com.chooblarin.githublarin.api.auth.Credential;
+import com.chooblarin.githublarin.api.client.GitHubClient;
 import com.chooblarin.githublarin.databinding.FragmentEventBinding;
+import com.chooblarin.githublarin.model.Entry;
+import com.chooblarin.githublarin.model.FeedParser;
 import com.chooblarin.githublarin.ui.adapter.EventAdapter;
 import com.chooblarin.githublarin.ui.listener.OnItemClickListener;
+import com.squareup.okhttp.Credentials;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.trello.rxlifecycle.FragmentEvent;
 import com.trello.rxlifecycle.components.support.RxFragment;
+
+import java.io.IOException;
+import java.util.List;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 public class EventFragment extends RxFragment implements OnItemClickListener {
 
     EventAdapter eventAdapter;
+    GitHubClient gitHubClient;
     FragmentEventBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         eventAdapter = new EventAdapter(getActivity());
+
+        Context context = getActivity().getApplicationContext();
+        String username = Credential.username(context);
+        String password = Credential.password(context);
+
+        String authorization = null;
+        if (!TextUtils.isEmpty(username) && !TextUtils.isEmpty(password)) {
+            authorization = Credentials.basic(username, password);
+        }
+        gitHubClient = ApiClientProvider.gitHubClient(authorization);
     }
 
     @Nullable
@@ -38,6 +69,42 @@ public class EventFragment extends RxFragment implements OnItemClickListener {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         setupEventListView(binding.recyclerviewEvent);
+        gitHubClient.feeds()
+                .map(feedsResponse -> feedsResponse.currentUserUrl)
+                .flatMap(url -> Observable.create((Observable.OnSubscribe<Response>) subscriber -> {
+                    Request request = new Request.Builder().url(url).build();
+                    try {
+                        Response response = new OkHttpClient().newCall(request).execute(); // todo
+                        subscriber.onNext(response);
+                    } catch (IOException e) {
+                        subscriber.onError(e);
+                    }
+                    subscriber.onCompleted();
+                }))
+                .map(response -> {
+                    String bodyText = null;
+                    try {
+                        bodyText = response.body().string();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return bodyText;
+                })
+                .map(FeedParser::parseString)
+                .compose(this.<List<Entry>>bindUntilEvent(FragmentEvent.STOP))
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<List<Entry>>() {
+                    @Override
+                    public void call(List<Entry> entries) {
+                        eventAdapter.addAll(entries);
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        throwable.printStackTrace();
+                    }
+                });
     }
 
     @Override
