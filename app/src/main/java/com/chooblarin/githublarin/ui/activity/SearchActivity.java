b02c06092c71ac893c7bb8cc1f67bf668ffcbf77
chooblarin <choo.bla.rin@gmail.com>
2016-01-08 09:49:34 +0900

    Add SearchActivity

---
+++ app/src/main/java/com/chooblarin/githublarin/ui/activity/SearchActivity.java
@@ -0,0 +1,8 @@
+package com.chooblarin.githublarin.ui.activity;
+
+public class SearchActivity extends BaseActivity {
+    @Override
+    protected void setupComponent() {
+
+    }
+}
--- app/src/main/java/com/chooblarin/githublarin/ui/activity/StartupActivity.java
+++ app/src/main/java/com/chooblarin/githublarin/ui/activity/StartupActivity.java
@@ -1,6 +1,5 @@
 package com.chooblarin.githublarin.ui.activity;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.widget.Toast;
