9e04aea82ebed26f2db92e86b7248ba7f55b5bc9
chooblarin <choo.bla.rin@gmail.com>
2016-02-09 22:24:35 +0900

    Demo like button
    mimicking this amazing [article](http://frogermcs.github.io/twitters-like-animation-in-android-alternative/)

---
+++ app/src/main/java/com/chooblarin/githublarin/ui/widget/likebutton/LikeButtonView.java
@@ -0,0 +1,160 @@
+package com.chooblarin.githublarin.ui.widget.likebutton;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.chooblarin.githublarin.R;
+
+/**
+ * @see <a href="http://frogermcs.github.io/twitters-like-animation-in-android-alternative/">Twitter's like animation in Android - alternative</a>
+ */
+public class LikeButtonView extends FrameLayout implements View.OnClickListener {
+
+    private static final DecelerateInterpolator DECELERATE_INTERPOLATOR = new DecelerateInterpolator();
+    private static final AccelerateDecelerateInterpolator ACCELERATE_DECELERATE_INTERPOLATOR = new AccelerateDecelerateInterpolator();
+    private static final OvershootInterpolator OVERSHOOT_INTERPOLATOR = new OvershootInterpolator(4);
+
+    ImageView starImageView;
+    DotsView dotsView;
+    CircleView circleView;
+
+    private boolean isChecked;
+    private AnimatorSet animatorSet;
+
+    public LikeButtonView(Context context) {
+        super(context);
+        init();
+    }
+
+    public LikeButtonView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public LikeButtonView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public LikeButtonView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        LayoutInflater.from(getContext()).inflate(R.layout.view_like_button, this, true);
+        circleView = (CircleView) findViewById(R.id.view_circle);
+        dotsView = (DotsView) findViewById(R.id.view_dots);
+        starImageView = (ImageView) findViewById(R.id.image_star_like);
+        setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        isChecked = !isChecked;
+        starImageView.setImageResource(isChecked ? R.drawable.ic_star_rate_on : R.drawable.ic_star_rate_off);
+
+        if (animatorSet != null) {
+            animatorSet.cancel();
+        }
+
+        if (isChecked) {
+            starImageView.animate().cancel();
+            starImageView.setScaleX(0);
+            starImageView.setScaleY(0);
+            circleView.setInnerCircleRadiusProgress(0);
+            circleView.setOuterCircleRadiusProgress(0);
+            dotsView.setCurrentProgress(0);
+
+            animatorSet = new AnimatorSet();
+
+            ObjectAnimator outerCircleAnimator = ObjectAnimator.ofFloat(circleView, CircleView.OUTER_CIRCLE_RADIUS_PROGRESS, 0.1f, 1f);
+            outerCircleAnimator.setDuration(250);
+            outerCircleAnimator.setInterpolator(DECELERATE_INTERPOLATOR);
+
+            ObjectAnimator innerCircleAnimator = ObjectAnimator.ofFloat(circleView, CircleView.INNER_CIRCLE_RADIUS_PROGRESS, 0.1f, 1f);
+            innerCircleAnimator.setDuration(200);
+            innerCircleAnimator.setStartDelay(200);
+            innerCircleAnimator.setInterpolator(DECELERATE_INTERPOLATOR);
+
+            ObjectAnimator starScaleYAnimator = ObjectAnimator.ofFloat(starImageView, ImageView.SCALE_Y, 0.2f, 1f);
+            starScaleYAnimator.setDuration(350);
+            starScaleYAnimator.setStartDelay(250);
+            starScaleYAnimator.setInterpolator(OVERSHOOT_INTERPOLATOR);
+
+            ObjectAnimator starScaleXAnimator = ObjectAnimator.ofFloat(starImageView, ImageView.SCALE_X, 0.2f, 1f);
+            starScaleXAnimator.setDuration(350);
+            starScaleXAnimator.setStartDelay(250);
+            starScaleXAnimator.setInterpolator(OVERSHOOT_INTERPOLATOR);
+
+            ObjectAnimator dotsAnimator = ObjectAnimator.ofFloat(dotsView, DotsView.DOTS_PROGRESS, 0, 1f);
+            dotsAnimator.setDuration(900);
+            dotsAnimator.setStartDelay(50);
+            dotsAnimator.setInterpolator(ACCELERATE_DECELERATE_INTERPOLATOR);
+
+            animatorSet.playTogether(
+                    outerCircleAnimator,
+                    innerCircleAnimator,
+                    starScaleYAnimator,
+                    starScaleXAnimator,
+                    dotsAnimator
+            );
+
+            animatorSet.addListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationCancel(Animator animation) {
+                    circleView.setInnerCircleRadiusProgress(0);
+                    circleView.setOuterCircleRadiusProgress(0);
+                    dotsView.setCurrentProgress(0);
+                    starImageView.setScaleX(1);
+                    starImageView.setScaleY(1);
+                }
+            });
+
+            animatorSet.start();
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                starImageView.animate().scaleX(0.7f).scaleY(0.7f).setDuration(150).setInterpolator(DECELERATE_INTERPOLATOR);
+                setPressed(true);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                float x = event.getX();
+                float y = event.getY();
+                boolean isInside = (x > 0 && x < getWidth() && y > 0 && y < getHeight());
+                if (isPressed() != isInside) {
+                    setPressed(isInside);
+                }
+                break;
+
+            case MotionEvent.ACTION_UP:
+                starImageView.animate().scaleX(1).scaleY(1).setInterpolator(DECELERATE_INTERPOLATOR);
+                if (isPressed()) {
+                    performClick();
+                    setPressed(false);
+                }
+                break;
+        }
+        return true;
+    }
+}
